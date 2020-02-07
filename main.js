82399ed5fd896382a3550fc2c6d63c8fdf8cc780
oneiros666 <r.cosenza39@gmail.com>
2020-02-07 05:50:24 -0500

    confused myself

--- main.js
+++ main.js
@@ -4,17 +4,15 @@ const APP_TOKEN = '5VOV1x0FuTC7doEV49vIZUGD5';
 let dataDiv = document.querySelector("#tree_data");
 dataDiv.innerHTML = '';
 let treeObjects = new Object();
-let trees = new Array();
-
-// function generateSuccessHTMLOutput(response) {
-//   return '<h4>Result</h4>' +
-//     // '<h5>Data:</h5> ' +
-//     // '<pre>' + response.data + ' ' + response.dataText + '</pre>' +
-//     //'<h5>Headers:</h5>' +
-//     //'<pre>' + JSON.stringify(response.headers, null, '\t') + '</pre>' +
-//     '<h5>Data:</h5>' +
-//     '<pre>' + JSON.stringify(response.data, null, '\t') + '</pre>';
-// }
+//let treeKeys = new O();
+let trees = new Set();
+let treez = new Set();
+let treeVals = new Object();
+let treeMap = new Map();
+let arbor = new Object();
+let arborMap = new Map();
+let wood = new Array();
+let obj = {};
 
 let response = async function () {
   await axios.get(`${BASE_URL}?$$app_token=`,
@@ -22,70 +20,77 @@ let response = async function () {
       "X-App-Token": APP_TOKEN
     }).then(res => {
       Object.assign(treeObjects, res.data);
-      //console.log(treeObjects);
       const iterate = (treeObjects) => {
         Object.keys(treeObjects).forEach(key => {
 
           //console.log(`key: ${key}, value: ${treeObjects[key]}`)
-          console.log(treeObjects[key]);
-          //trees.push(`key: ${key}, value: ${treeObjects[key]}`);
+
+          (key == 'spc_common') ? trees.add(treeObjects[key]) : null;
+          (key == 'spc_latin') ? treez.add(treeObjects[key]) : null;
+          //console.log(treeObjects[key]);
+          //switch (key) {
+          //case 'spc_common': trees.add(treeObjects[key]);
+          // break;
+          //case 'spc_latin': treez.add(treeObjects[key]);
+          //break;
+          //default: console.log([key, treeObjects[key]]);
+          //}          
+          console.log(treeObjects[key].size);
+
           if (typeof treeObjects[key] === 'object') {
-            iterate(treeObjects[key])
+            Object.assign(obj, treeObjects[key]);
+
+            iterate(treeObjects[key]);
+
           }
         })
       }
-      console.log(iterate(treeObjects));
+      iterate(treeObjects);
+      console.log(obj)
+      console.log(arborMap.get("tree_id"));
+      //arbor += Object.fromEntries(treeObjects);
 
-      //console.log(trees);
+      //console.log(arbor);
 
-      //for (let i = 0; i < treeObjects.length; i++) {
-      //dataDiv.innerHTML += `<p>${treeObjects[i].tree_id}</br>${treeObjects[i].spc_common}</p>`;
+      let duir = Array.from(trees.values());
+      let oak = Array.from(treez.values());
+      for (let i = 0; i < duir.length; i++) {
+        //console.log(duir[i]);
+        treeMap.set(duir[i], oak[i]);
+        dataDiv.innerHTML += `<div class="trees"><h2 id="${duir[i]}"><a href="#">${duir[i]}</a></br><h3>${oak[i]}</h3></h2><div>`;
 
-      //console.log(treeObjects[i]);
-      //trees.push(treeObjects[i]);
-      //for (let j = 0; j < trees.length; j++) {
-      //console.log(Object.entries(trees[j]));
-      //}
-      //let treez = new String(trees.spc_common);
-      //console.log(treez);
-      //let name = Object.values(treeObjects[i]);
-      //console.log(treeObjects[i].spc_common);
-      //let maples = /maple/;
-      //console.log(trees.matchAll(maples))
-      //console.log(redMaple.test(treeObjects[i].spc_common));
-      // let norMaple = /Norway maple/;
-      // console.log(norMaple.test(treeObjects[i].spc_common));
-      //}
-      //let name = treeObjects.forEach().map(a => a.spc_common);
-      //trees.push(name);
-      //console.log(trees.values());
-      //console.log(treeObjects);
-      //for (const [index, element] of treeObjects.entries('spc_common')) {
-      // console.log(index, element);
-      //}
-      //for (const prop in treeObjects) {
-      //const info = Object.entries(treeObjects[prop]);
-      // console.log(info.values);
-      //for (const index in info) {
-      //console.log(info[index].spc_common);
-      //}
-      //trees.add(treeObjects[prop].spc_common);
-      //console.log(Object.values(treeObjects[prop]));
+      }
+      // const reIterate = (treeObjects) => {
+      //   Object.keys(treeObjects).forEach(key => {
+      //     console.log(arbor);
 
+      //     if (typeof treeObjects[key] === 'object') {
+      //       reIterate(treeObjects[key]);
+      //     }
+      //   })
+      // }
+      //console.log(reIterate()); //} // console.log(treeMap)
+
+      // for (let j = 0; j < arborMap.size; j++) {
+      //console.log(arborMap.get("tree_id"));
       //}
-      //console.log(trees);
-      //for (let name of trees) {
-      //console.log(name);
-      // dataDiv.innerHTML += `<div><a href="#">${name}</a></div>`;
-      //}
+      // arborMap.forEach(key => {
+      //   console.log(arborMap[key]);
+
+      // })
+      for (let [k, v] of arborMap.entries()) {
+        //console.log(...[v]);
+        //(k == 'tree_id') ? console.log(...[v]) : console.log(...[k]);
+        //Object.defineProperty(k, v);
+        //console.log(obj);
+      }
     }).catch(err => {
       console.log(err);
     })
 }
-response()
-
-function treeCategories(set) {
-  for (let i = 0; i < set.length; i++) {
-    dataDiv.innerHTML += `<p>${treez[i]}</p>`
-  }
-}
+response();
+//function treeCategories(set) {
+  //for (let i = 0; i < set.length; i++) {
+    //dataDiv.innerHTML += `<p>${treez[i]}</p>`
+  //}
+//}
